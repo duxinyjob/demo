c63f53f303a71a57dd7c49ec3705ca30c3fc154e
duxinyu <418714451@qq.com>
2021-01-01 19:42:46 +0800

    install

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+
+@EnableTransactionManagement
+@MapperScan("com.carson.cachedemo.mapper")
+@EnableScheduling
+@EnableCaching
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
